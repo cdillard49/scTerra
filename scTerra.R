a9fe6f14ab1c45d1603ca8ae92bb0b0e7d520d32
Christian Dillard <70979511+cdillard49@users.noreply.github.com>
2022-05-11 12:52:13 -0400

    work

--- scTerra.R
+++ scTerra.R
@@ -3,6 +3,7 @@ library(tidyr)
 library(dplyr)
 setwd("~/GitHub/scTerra")
 
+end_reads_50 <- read.delim("inputs/end_reads_barcodes50")
 express_mat_51 <- read.delim("inputs/SRR10018151_GE_matrix_filtered.txt", row.names = 1)
 #1802 cell barcodes
 redi_cos_51 <- read.delim("inputs/SRR10018151_ind_ss_redi_cos.txt")
@@ -11,8 +12,24 @@ redi_cos_51 <- read.delim("inputs/SRR10018151_ind_ss_redi_cos.txt")
 barcodelist <- colnames(express_mat_51) 
 #list of barcodes to use for filtering
 
+
 redi_separate_51 <- separate_rows(redi_cos_51, Cells, convert = TRUE)
 #separate out/expand the Cells column
-#note this breaks the N_cells column now
+#note this breaks the N_cells column for now
 filt_redit_51<- redi_separate_51 %>% filter(Cells %in% barcodelist)
 #keep only cells found in the GE barcode list
+
+
+
+#DESEQ2
+pasCts <- system.file("extdata",
+                      "pasilla_gene_counts.tsv",
+                      package="pasilla", mustWork=TRUE)
+pasAnno <- system.file("extdata",
+                       "pasilla_sample_annotation.csv",
+                       package="pasilla", mustWork=TRUE)
+cts <- as.matrix(read.csv(pasCts,sep="\t",row.names="gene_id"))
+coldata <- read.csv(pasAnno, row.names=1)
+coldata <- coldata[,c("condition","type")]
+coldata$condition <- factor(coldata$condition)
+coldata$type <- factor(coldata$type)
