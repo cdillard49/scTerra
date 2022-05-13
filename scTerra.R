4d58c027b45428de830090aaa17cf4e76c0b9a7d
Christian Dillard <70979511+cdillard49@users.noreply.github.com>
2022-05-13 12:02:54 -0400

    commenting/touch-ups

--- scTerra.R
+++ scTerra.R
@@ -16,30 +16,49 @@ redi_cos_51 <- read.delim("inputs/SRR10018151_ind_ss_redi_cos.txt")
 #many snvss found in different cells
 barcodelist <- colnames(express_mat_51) 
 #list of barcodes to use for filtering
-redi_separate_51 <- separate_rows(redi_cos_51, Cells, convert = TRUE)
+filt_redi_51 <- subset(redi_cos_51, subset=(N_cells >= 10))
+#remove low cell count (10)
+redi_separate_51 <- separate_rows(filt_redi_51, Cells, convert = TRUE)
 #separate out/expand the Cells column
 #note this breaks the N_cells column for now
-filt_redit_51<- redi_separate_51 %>% filter(Cells %in% barcodelist)
+filt_sub_redi_51 <- redi_separate_51 %>% filter(Cells %in% barcodelist)
 #keep only cells found in the GE barcode list
 
 
+#DESEQ2 GE using cells with specific SNVs
+#line 30: change the SNV to the desired SNV of choice
+snv <- filt_sub_redi_51[filt_sub_redi_51$SNV %in% c("1:100291620_T>G"), ]
+snv_cells_A <- snv$Cells
+#A is chosen SNV
+snv_cells_B <- barcodelist[!barcodelist %in% snv_cells_A]
+#B is all other SNVs
+#filtering out only those snvs selected and setting the condition for dseq as A
+snv_cells_A <- as.data.frame(snv_cells_A)
+snv_cells_B <- as.data.frame(snv_cells_B)
+snv_cells_A$condition <- "A"
+snv_cells_B$condition <- "B"
+names(snv_cells_A)[1] <- "barcode"
+names(snv_cells_B)[1] <- "barcode"
+dseq_meta <- rbind(snv_cells_A, snv_cells_B)
+#binding the two conditional dataframes together 
+dseq_meta$condition <- as.factor(dseq_meta$condition)
 
-#DESEQ2 GE using random cells (WIP: will use cells with specific scTerra SNVs)
-letters <- LETTERS[1:2]
-fac <- sample(letters, 1802, replace = TRUE)
-metadata_51 <- data.frame(
-  sample = barcodelist,
-  condition = fac,
-  row.names = "sample")
-metadata_51$condition <- as.factor(metadata_51$condition)
-#metadata creation, change the condition column to match SNV condition
+coldata <- data.frame(barcode = barcodelist, row.names = "barcode")
+dseq_meta_index <- match(rownames(coldata), dseq_meta$barcode)
+#create the GE matrix barcode dataframe for reordering 
+#match the GE barcode to the metadata barcode and get an index for reordering
+dseq_meta_reorder <- as.data.frame(dseq_meta[dseq_meta_index, ])
+dseq_meta_reorder <- data.frame(dseq_meta_reorder, row.names = "barcode")
+#reordering and dataframe manipulation to get into deseq2 format
 
-dds <- DESeqDataSetFromMatrix(countData = express_mat_51, colData = metadata_51, 
+#metadata creation
+
+dds <- DESeqDataSetFromMatrix(countData = express_mat_51, colData = dseq_meta_reorder, 
                               design = ~ condition)
 #create the deseq object
-dds$condition <- relevel(dds$condition, ref = "A")
-#set condition
-#here we set as A, but in the future will set condition appropriately
+dds$condition <- relevel(dds$condition, ref = "B")
+#set reference condition
+#here we set B reference/control
 dds <- DESeq(dds)
 #initialize the differential expression
 res <- results(dds, independentFiltering = FALSE)
@@ -53,7 +72,7 @@ normalized_counts <- counts(dds, normalized=TRUE)
 head(normalized_counts)
 #view the normalized counts
 
-resLFC<- lfcShrink(dds, coef="condition_B_vs_A", type = "normal", lfcThreshold = 1, alpha = 0.05)
+resLFC<- lfcShrink(dds, coef="condition_A_vs_B", type = "normal", lfcThreshold = 1, alpha = 0.05)
 plotMA(resLFC, ylim=c(-2,2), cex=.4)
 plotMA(res, ylim=c(-2,2),cex=.4)
 #visualize with MA plot
