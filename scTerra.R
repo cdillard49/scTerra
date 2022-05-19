39ebf80ad55546b9139f22ff035852f02aadfc5c
Christian Dillard <70979511+cdillard49@users.noreply.github.com>
2022-05-19 15:48:51 -0400

    Add input functionality and example inputs

--- scTerra.R
+++ scTerra.R
@@ -1,40 +1,41 @@
-require(data.table)
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
 BiocManager::install("DESeq2")
+require(data.table)
 library(tidyr)
 library(dplyr)
 library(DESeq2)
 library(EnhancedVolcano)
 setwd("~/GitHub/scTerra")
-#end_reads_50 <- read.delim("inputs/end_reads_barcodes50")
-
-snv_input <- read.delim("example.txt/tsv") ##Not Ran
+sample_name <- "SRR10018151"
+#use to generate variable for naming later, only use sample name that matches
+# the one from line 19
+snv_input <- read.delim("inputs/example.txt")
 #file needs to have column with SNV and row with SNV location/coordinates
-snv_list <- snv_input$SNV ##Not Ran
-
+snv_list <- snv_input$SNV 
+#end_reads_50 <- read.delim("inputs/end_reads_barcodes50")
 express_mat_51 <- read.delim("inputs/SRR10018151_GE_matrix_filtered.txt", row.names = 1)
-#1802 cell barcodes
 redi_cos_51 <- read.delim("inputs/SRR10018151_ind_ss_redi_cos.txt")
-#17210 snvs each with corresponding cell barcodes
-#many snvss found in different cells
 barcodelist <- colnames(express_mat_51) 
 #list of barcodes to use for filtering
-----
-#redi_cos_51 <- subset(redi_cos_51, subset=(N_cells >= 10))
-#remove low cell count (10)
-----
+
+##redi_cos_51 <- subset(redi_cos_51, subset=(N_cells >= 10))
+##remove low cell count (10)
+
 redi_separate_51 <- separate_rows(redi_cos_51, Cells, convert = TRUE)
 #separate out/expand the Cells column
 #note this breaks the N_cells column for now
 filt_sub_redi_51 <- redi_separate_51 %>% filter(Cells %in% barcodelist)
 #keep only cells found in the GE barcode list
 
+####
+
 
 #DESEQ2 GE using cells with specific SNVs
-#line 33: change the SNV to the desired SNV of choice
-snv <- filt_sub_redi_51[filt_sub_redi_51$SNV %in% c("1:100291620_T>G"), ]
+for (input in snv_list) {
+  
+snv <- filt_sub_redi_51[filt_sub_redi_51$SNV %in% input, ]
 snv_cells_A <- snv$Cells
 #A is chosen SNV
 snv_cells_B <- barcodelist[!barcodelist %in% snv_cells_A]
@@ -57,7 +58,6 @@ dseq_meta_index <- match(rownames(coldata), dseq_meta$barcode)
 dseq_meta_reorder <- as.data.frame(dseq_meta[dseq_meta_index, ])
 dseq_meta_reorder <- data.frame(dseq_meta_reorder, row.names = "barcode")
 #reordering and dataframe manipulation to get into deseq2 format
-
 #metadata creation
 
 dds <- DESeqDataSetFromMatrix(countData = express_mat_51, colData = dseq_meta_reorder, 
@@ -78,15 +78,46 @@ normalized_counts <- counts(dds, normalized=TRUE)
 #normalize counts variable
 head(normalized_counts)
 #view the normalized counts
+output_gene <- as.data.frame(res[order(res$log2FoldChange, decreasing=TRUE),])
+#output genes ordered by upregulated genes, set decreasing=FALSE for downregulated
+
+file_name_genes <- paste0("outputs/", sample_name, "_DE_genes_", input, ".tsv")
+file_name_genes <- gsub(":|>", "_", file_name_genes)
+
+file_name_plotMA_shrink <- paste0("outputs/", sample_name, "_DE_MA_Shrink_", input, ".pdf")
+file_name_plotMA_shrink <- gsub(":|>", "_",file_name_plotMA_shrink)
+
+file_name_plotMA <- paste0("outputs/", sample_name, "_DE_MA_", input, ".pdf")
+file_name_plotMA <- gsub(":|>", "_", file_name_plotMA)
+
+file_name_volcano_shrink <- paste0("outputs/", sample_name, "_DE_Volcano_", input, ".pdf")
+file_name_volcano_shrink <- gsub(":|>", "_", file_name_volcano_shrink)
+#file name format for all plots and gene table, removing ":" and ">"
+write.table(x = output_gene, file = file_name_genes, quote = FALSE, sep = "\t")
+#write gene table to file
 
 resLFC<- lfcShrink(dds, coef="condition_A_vs_B", type = "normal", lfcThreshold = 1)
+pdf(file = file_name_plotMA_shrink)
 plotMA(resLFC, ylim=c(-2,2), cex=.4)
+dev.off()
+pdf(file = file_name_plotMA)
 plotMA(res, ylim=c(-2,2),cex=.4)
+dev.off()
+pdf(file = file_name_volcano_shrink)
 EnhancedVolcano(resLFC, lab=rownames(resLFC), x="log2FoldChange", y= "pvalue", 
                 title = "Normal vs. SNV" ,xlab = bquote(~Log[2]~ "fold change"), 
                 colAlpha = .5, pointSize = 1.0, labSize = 3.0, col = c("black", "blue", "green", "red"), 
-                legendPosition = "right" )
+                legendPosition = "right",
+                legendLabSize = 10.0,
+                legendIconSize = 3.0)
+dev.off()
 
 #visualize with MA plot
 #logfold change shrinkage vs un-shrunk
 #volcanoPlots
+#write plots to pdf format
+
+}
+
+####
+print("DONE")
